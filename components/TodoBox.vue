7151314b434a0e2e14c8a8c4bbee05e23706101f
TakumiMatsuda0630 <mattunari0630@icloud.com>
2025-02-15 15:16:42 +0900

    update: 一覧ページで表示するタイトルの文字数に上限を設定. またスマホ画面で期限を非表示に.

--- components/TodoBox.vue
+++ components/TodoBox.vue
@@ -12,7 +12,7 @@
                 @click="changeDisplayDescription"
             >
             </span>
-            <span class="title">{{ props.todo.title }}</span>
+            <span class="title">{{ displayTitle }}</span>
             <span class="dead-line">{{ displayDeadLine }}</span>
             <div class="icons">
                 <NuxtLink :to="`/edit/${props.todo.id}`">
@@ -49,6 +49,15 @@ interface Props {
     todo: Todo,
 };
 const props = defineProps<Props>();
+
+const MAX_TITLE_LENGTH: number = 15;
+const displayTitle: ComputedRef<string> = computed(() => {
+    const displayTitle: string = props.todo.title;
+    if (displayTitle.length > MAX_TITLE_LENGTH){
+        return displayTitle.substring(0, MAX_TITLE_LENGTH) + '...';
+    }
+    return displayTitle;
+});
 
 const displayDeadLine: ComputedRef<string> = computed(() => {
     return props.todo.deadLine.replaceAll('-', '/');
